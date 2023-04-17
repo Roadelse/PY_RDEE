83fd01b1b25bcde9ca687697d7a21cb6762afeae
Roadelse <roadelse@zhejianglab.com>
2023-04-17 16:20:15 +0800

    v1.0, init

---
+++ rdee_code.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+#**********************************************************************
+# this function is used to perform pretty and prescribed skeleton comments
+#**********************************************************************
+def norm_skComments(C, level = 1, language = 'ncl') : # normalize skeleton comments
+    import sys
+
+    if language == 'ncl' :
+        cl = ';' # comment label
+    elif language == 'python':
+        cl = '#'
+    else:
+        print('unknwon language : {}'.format(language))
+        sys.exit(1)
+
+    if level == 1: # final 60
+        charL = '>'
+        charR = '<'
+        lenCharL = ((59 - 4) - len(C)) // 2
+        lenCharR = 59 - 4 - len(C) - lenCharL
+        res = "{} {} {} {}".format(cl, charL * lenCharL, C, charR * lenCharR)
+    elif level == 2:
+        res = "{} {} {}".format(cl, '=' * 15, C)
+    elif level == 3:
+        res = "{} {} {}".format(cl, '~' * 10, C)
+    elif level == 4:
+        res = "{} {} {}".format(cl, '-' * 5, C)
+
+    print(res)
+
---
+++ rdee_win.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+
+
+#**********************************************************************
+# this function is used to create shortcut
+#**********************************************************************
+def createShortCut(filename2, lnkname, debug = False):
+    # filename2 : should be abspath, or there will be some strange errors
+    # from https://www.cnblogs.com/luoheng23/p/11342479.html
+
+    import platform
+    assert platform.system() == 'Windows', 'function <createShortCut> : this function could only be run in Windows system! Now in {}'.format(platform.system())
+
+    import os
+    import win32com.client as client
+
+    shell = client.Dispatch("WScript.Shell")
+
+    filename = os.path.abspath(filename2)
+
+    if debug : 
+        print("filename = {}".format(filename))
+        print("link name = {}".format(lnkname))
+    
+    shortcut = shell.CreateShortCut(lnkname)
+    shortcut.TargetPath = filename
+    shortcut.save()
+
+
+#**********************************************************************
+# this function is used to get shortcut target path
+#**********************************************************************
+def GetShortCut(shortcut):
+    # from https://www.cnblogs.com/luoheng23/p/11342479.html
+
+    import platform
+    assert platform.system() == 'Windows', 'function <createShortCut> : this function could only be run in Windows system! Now in {}'.format(platform.system())
+
+    import win32com.client as client
+
+    shell = client.Dispatch("WScript.Shell")
+
+    return shell.CreateShortCut(shortcut).Targetpath
